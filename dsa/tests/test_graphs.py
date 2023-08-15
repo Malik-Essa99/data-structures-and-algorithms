3fee246ada0009ff47c79fbb729c492412fff5dc
Malik-Essa99 <malikhodrob99@gmail.com>
2023-08-15 19:35:49 +0000

    CC 37 Done

--- dsa/tests/test_graphs.py
+++ dsa/tests/test_graphs.py
@@ -1,12 +1,13 @@
 import pytest 
 from collections import deque
-from dsa.graph import Graph
+from dsa.graph import Graph,business_trip
 
+@pytest.mark.skip(reason="Done")
 def test_add_vertex():
     graph = Graph()
     v1 = graph.add_vertix("A")
     assert v1 in graph.get_vertices()
-
+@pytest.mark.skip(reason="Done")
 def test_add_edge():
     graph = Graph()
     v1 = graph.add_vertix("A")
@@ -16,7 +17,8 @@ def test_add_edge():
     assert len(neighbors) == 1
     assert neighbors[0].vertix == v2
     assert neighbors[0].weight == 5
-
+    
+@pytest.mark.skip(reason="Done")
 def test_get_vertices():
     graph = Graph()
     v1 = graph.add_vertix("A")
@@ -26,6 +28,7 @@ def test_get_vertices():
     assert v1 in vertices
     assert v2 in vertices
 
+@pytest.mark.skip(reason="Done")
 def test_get_neighbors():
     graph = Graph()
     v1 = graph.add_vertix("A")
@@ -39,7 +42,8 @@ def test_get_neighbors():
     assert neighbors[0].weight == 5
     assert neighbors[1].vertix == v3
     assert neighbors[1].weight == 7
-
+    
+@pytest.mark.skip(reason="Done")
 def test_size():
     graph = Graph()
     v1 = graph.add_vertix("A")
@@ -48,16 +52,41 @@ def test_size():
     v3 = graph.add_vertix("C")
     assert graph.size() == 3
 
+@pytest.mark.skip(reason="Done")
 def test_single_vertex_edge():
     graph = Graph()
     v1 = graph.add_vertix("A")
     assert graph.size() == 1
     graph.add_edge(v1, v1)
     neighbors = graph.get_neighbors(v1)
-    assert len(neighbors) == 1  
+    assert len(neighbors) == 2 
     assert neighbors[0].vertix == v1
 
 
 ######################### buisness trip #########################
-#                       still wokring on it                     #
-######################### buisness trip #########################
+
+# @pytest.mark.skip(reason="Done")
+
+def test_cases():
+    g = Graph()
+    Pandora = g.add_vertix('Pandora')
+    Metroville = g.add_vertix('Metroville')
+    Arendelle = g.add_vertix('Arendelle')
+    Naboo = g.add_vertix('Naboo')
+    Monstropolis = g.add_vertix('Monstropolis')
+    Narnia = g.add_vertix('Narnia')
+    
+    g.add_edge(Metroville,Pandora,82)
+    g.add_edge(Metroville,Arendelle,99)
+    g.add_edge(Metroville,Narnia,37)
+    g.add_edge(Metroville,Naboo,26)
+    g.add_edge(Metroville,Monstropolis,105)
+    g.add_edge(Pandora,Arendelle,150)
+    g.add_edge(Arendelle,Monstropolis,42)
+    g.add_edge(Naboo,Monstropolis,73)
+    g.add_edge(Narnia,Naboo,250)
+    
+    assert business_trip(g,["Metroville", "Pandora"]) == 82
+    assert business_trip(g,["Arendelle","Monstropolis", "Naboo"]) == 115
+    assert business_trip(g,["Naboo", "Pandora"]) == None
+    assert business_trip(g,["Narnia", "Arendelle", "Naboo"]) == None
